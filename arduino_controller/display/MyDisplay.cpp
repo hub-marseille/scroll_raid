06832dbd017c549247bc553df2e49959254b5228
Kyalma <leoraphael.poulin@epitech.eu>
2017-02-12 19:12:18 +0100

    stable

---
+++ arduino_controller/display/MyDisplay.cpp
@@ -0,0 +1,102 @@
+// 
+// salut.
+// 
+
+#include "MyDisplay.h"
+
+ static byte  heart[3][8] = {
+	 { B00111000, B01000100, B01000010, B00100001, B00100001, B01000010, B01000100, B00111000 },   // empty
+	/*
+	00000000
+	01100110
+	10011001
+	10000001
+	01000010
+	00100100
+	00011000
+	*/
+	 { B00111000, B01000100, B01000010, B00100001, B00111111, B01111110, B01111100, B00111000 },   // half
+	/*
+	00000000
+	01100110
+	10011111
+	10001111
+	10001111
+	01001110
+	00101100
+	00011000
+	*/
+	 { B00111000, B01111100, B01111110, B00111111, B00111111, B01111110, B01111100, B00111000 } }; // full
+	/*
+	00000000
+	01100110
+	11111111
+	11111111
+	11111111
+	01111110
+	00111100
+	00011000
+	*/
+
+MyDisplay::MyDisplay()
+	: mMatrix(MATRIX_DIN, MATRIX_CLK, MATRIX_CS, MATRIX_COUNT)
+{
+	fillCount = true;
+	for (short it = 0; it < MATRIX_COUNT; ++it) {
+		mMatrix.clearDisplay(it);
+		mMatrix.setIntensity(it, LED_INTENSITY);
+		mMatrix.shutdown(it, false);
+	}
+	printByteArray(0, heart[0]);
+	printByteArray(1, heart[0]);
+	printByteArray(2, heart[0]);
+	printNumber(0);
+}
+
+MyDisplay::~MyDisplay()
+{
+}
+
+void	MyDisplay::printByteArray(int matrixIt, byte data[8]) {
+	if (matrixIt > MATRIX_COUNT - 1)
+		return;
+	mMatrix.setColumn(matrixIt, 0, data[0]);
+	mMatrix.setColumn(matrixIt, 1, data[1]);
+	mMatrix.setColumn(matrixIt, 2, data[2]);
+	mMatrix.setColumn(matrixIt, 3, data[3]);
+	mMatrix.setColumn(matrixIt, 4, data[4]);
+	mMatrix.setColumn(matrixIt, 5, data[5]);
+	mMatrix.setColumn(matrixIt, 6, data[6]);
+	mMatrix.setColumn(matrixIt, 7, data[7]);
+}
+
+bool	MyDisplay::printNumber(long value)
+{
+	bool  isNeg = false;
+
+	if (value < -9999999 || value > 99999999)
+		return (false);
+	if (value < 0) {
+		value *= -1;
+		isNeg = true;
+	}
+	printDigit(value, DIGIT_COUNT - 1, isNeg);
+	return (true);
+}
+
+void	MyDisplay::printDigit(long value, int offset, bool isNeg)
+{
+	mMatrix.setDigit(SCORE_MATRIX, offset, (byte)(value % 10), false);
+	if (value / 10 > 0)
+		printDigit(value / 10, offset - 1, isNeg);
+	else
+	{
+		if (fillCount)
+			while (offset > 0 && !(offset == 1 && isNeg)) {
+				mMatrix.setDigit(SCORE_MATRIX, offset - 1, 0, false);
+				offset -= 1;
+			}
+		if (isNeg)
+			mMatrix.setChar(SCORE_MATRIX, offset - 1, (byte)'-', false);
+	}
+}
---
+++ arduino_controller/display/MyDisplay.h
@@ -0,0 +1,42 @@
+// display.h
+
+#ifndef _DISPLAY_h
+#define _DISPLAY_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include <LedControl.h>
+
+#define		MATRIX_DIN	10
+#define		MATRIX_CS	16
+#define		MATRIX_CLK	14
+#define		MATRIX_COUNT	4
+#define		SCORE_MATRIX	3
+#define		DIGIT_COUNT		8
+
+#define MAX_MAX         0x4
+#define LED_INTENSITY   0x5
+#define NB_HEARTS       0x3
+#define NB_HEART_CELLS  0x8
+
+class MyDisplay
+{
+public:
+	MyDisplay();
+	~MyDisplay();
+	bool	printNumber(long value);
+
+private:
+	LedControl	mMatrix;
+	bool		fillCount;
+
+	void	printDigit(long value, int offset, bool isNeg);
+	void	printByteArray(int matrixIt, byte array[8]);
+};
+
+#endif
+
